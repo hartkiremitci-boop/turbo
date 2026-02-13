b574dd0bbb0ef24c6296149f221841a48ecf77db
hartkiremitci-boop <hartkiremitci@gmail.com>
2026-02-13 16:54:31 +0300

    Revert "Organize premium theme CSS and PHP code"

--- 08-Siparis-Takibi/takip.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Sipariş Takibi Özelleştirmeleri
- */
-add_filter('woocommerce_account_menu_items', function($items) {
-    // Sipariş takibi linkini hesaba ekle veya düzenle
-    return $items;
-}, 20);
--- 11-Destek-Hatti/destek.php
+++
@@ -1,8 +0,0 @@
-<?php
-add_action('wp_footer', function() {
-    ?>
-    <a href="https://wa.me/905XXXXXXX" class="tot-whatsapp-float" target="_blank">
-        <i class="ast-icon-whatsapp"></i>
-    </a>
-    <?php
-});
